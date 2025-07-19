54fc1d63d34739ca5a17f0a9e3d6edf62918dd3a
Wei-Chiu Chuang <weichiu@apache.org>
2025-07-19 00:39:50 -0700

    Update baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://ratis.apache.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ratis-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
